04a81bd03b0f16fc25b9cffe92655b62d7e9aa75
Jan Kusy <jan.kusy@matee.cz>
2021-02-26 15:17:17 +0100

    ✨Add base flow UC

--- shared/src/commonMain/kotlin/cz/matee/devstack/kmp/shared/base/usecase/UseCaseResult.kt
+++ shared/src/commonMain/kotlin/cz/matee/devstack/kmp/shared/base/usecase/UseCaseResult.kt
@@ -3,6 +3,7 @@ package cz.matee.devstack.kmp.shared.base.usecase
 import cz.matee.devstack.kmp.shared.base.Result
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 
 /**
  * Abstract class for a Use Case (Interactor in terms of Clean Architecture).
@@ -18,4 +19,12 @@ abstract class UseCaseResult<in Params, out T : Any>(dispatcher: CoroutineDispat
 abstract class UseCaseResultNoParams<out T : Any>(dispatcher: CoroutineDispatcher = Dispatchers.Default) :
     UseCase<Unit, Result<T>>(dispatcher) {
     suspend operator fun invoke() = super.invoke(Unit)
+}
+
+abstract class UseCaseFlowResult<in Params, out T : Any>(dispatcher: CoroutineDispatcher = Dispatchers.Default) :
+    UseCase<Params, Flow<Result<T>>>(dispatcher)
+
+abstract class UseCaseFlowResultNoParams<out T : Any>(dispatcher: CoroutineDispatcher = Dispatchers.Default) :
+    UseCase<Unit, Flow<Result<T>>>(dispatcher) {
+    suspend operator fun invoke(): Flow<Result<T>> = super.invoke(Unit)
 }
