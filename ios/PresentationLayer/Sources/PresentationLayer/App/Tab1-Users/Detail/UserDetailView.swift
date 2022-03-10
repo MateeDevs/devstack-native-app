7d8f33ab9739de67ed81905b10bfcfdc11dc5805
Patrik Cesnek <patrik.cesnek@matee.cz>
2022-03-10 10:38:13 +0100

    Make refresh work and create initials image for user

--- ios/PresentationLayer/Sources/PresentationLayer/App/Tab1-Users/Detail/UserDetailView.swift
+++ ios/PresentationLayer/Sources/PresentationLayer/App/Tab1-Users/Detail/UserDetailView.swift
@@ -6,13 +6,57 @@
 import SwiftUI
 
 struct UserDetailView: View {
+    
+    @ObservedObject private var viewModel: UserDetailViewModel
+        
+    init(viewModel: UserDetailViewModel) {
+        self.viewModel = viewModel
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                RefreshControl(coordinateSpace: .named("RefreshControl")) {
+                    viewModel.intent(.onRefresh)
+                }
+                    .frame(height: 10)
+                VStack {
+                ZStack {
+                    Circle()
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(Color.yellow)
+                    Text(viewModel.state.user?.fullName.initials ?? "")
+                        .font(.title)
+                }
+                .padding()
+                Spacer(minLength: 40)
+                VStack {
+                    Text(viewModel.state.user?.fullName ?? "")
+                        .font(.title)
+                    Spacer()
+                    Text(viewModel.state.user?.bio ?? "")
+                }
+                }
+            }
+            .onAppear {
+                viewModel.intent(.onAppear)
+                viewModel.intent(.onRefresh)
+            }
+        }
+        .coordinateSpace(name: "RefreshControl")
     }
 }
 
-struct SwiftUIView_Previews: PreviewProvider {
+#if DEBUG
+import Resolver
+
+struct UserDetail_Previews: PreviewProvider {
     static var previews: some View {
-        UserDetailView()
+        Resolver.registerUseCasesForPreviews()
+        let vm = UserDetailViewModel(flowController: nil, userId: "")
+            return PreviewGroup {
+                UserDetailView(viewModel: vm)
+            }
     }
 }
+#endif
