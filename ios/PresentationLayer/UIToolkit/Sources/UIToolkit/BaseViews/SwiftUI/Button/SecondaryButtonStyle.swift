78953c8c6fcb81a7ed119d662c2bbfb76052dfac
David Sobíšek <dsobisek8@gmail.com>
2022-06-15 12:14:14 +0200

    Add loading and large state to secondaryButton, modify secondaryButton initialiser.

--- ios/PresentationLayer/UIToolkit/Sources/UIToolkit/BaseViews/SwiftUI/Button/SecondaryButtonStyle.swift
+++ ios/PresentationLayer/UIToolkit/Sources/UIToolkit/BaseViews/SwiftUI/Button/SecondaryButtonStyle.swift
@@ -7,21 +7,48 @@ import SwiftUI
 
 public struct SecondaryButtonStyle: ButtonStyle {
     
-    public init() {}
+    private let isLarge: Bool
+    private let isLoading: Bool
+    
+    public init(
+        isLarge: Bool = true,
+        isLoading: Bool = false
+    ) {
+        self.isLarge = isLarge
+        self.isLoading = isLoading
+    }
     
     public func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .font(AppTheme.Fonts.secondaryButton)
-            .foregroundColor(AppTheme.Colors.secondaryButtonTitle)
-            .padding()
+        VStack {
+            if isLoading {
+                ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle(tint: .black))
+            } else {
+                configuration.label
+                    .font(AppTheme.Fonts.secondaryButton)
+                    .foregroundColor(AppTheme.Colors.secondaryButtonTitle)
+                    .padding()
+            }
+        }
+        .frame(maxWidth: isLarge ? .infinity : nil, minHeight: 24)
+        .padding()
+        .cornerRadius(5)
     }
 }
 
 #if DEBUG
 struct SecondaryButtonStyle_Previews: PreviewProvider {
     static var previews: some View {
-        Button("Lorem Ipsum") {}
-            .buttonStyle(SecondaryButtonStyle())
+        VStack {
+            Button("Lorem Ipsum") {}
+                .buttonStyle(SecondaryButtonStyle())
+            Button("Lorem Ipsum") {}
+                .buttonStyle(SecondaryButtonStyle(isLoading: true))
+            Button("Lorem Ipsum") {}
+                .buttonStyle(SecondaryButtonStyle(isLarge: false))
+            Button("Lorem Ipsum") {}
+                .buttonStyle(SecondaryButtonStyle(isLarge: false, isLoading: true))
+        }
     }
 }
 #endif
