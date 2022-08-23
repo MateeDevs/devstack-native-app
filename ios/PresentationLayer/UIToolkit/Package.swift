d460a5949ec4bd3ed7788844a289da5d14bd45bf
Petr Chmelar <petr.chmelar@matee.cz>
2022-08-23 16:48:14 +0200

    Use SwiftGen via SPM Plugin

--- ios/PresentationLayer/UIToolkit/Package.swift
+++ ios/PresentationLayer/UIToolkit/Package.swift
@@ -17,7 +17,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "Utilities", path: "../DomainLayer/Utilities")
+        .package(name: "Utilities", path: "../DomainLayer/Utilities"),
+        .package(url: "https://github.com/SwiftGen/SwiftGenPlugin", .upToNextMajor(from: "6.6.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -26,6 +27,14 @@ let package = Package(
             name: "UIToolkit",
             dependencies: [
                 .product(name: "Utilities", package: "Utilities")
+            ],
+            exclude: [
+                "swiftgen-strings.stencil",
+                "swiftgen-xcassets.stencil",
+                "swiftgen.yml"
+            ],
+            plugins: [
+              .plugin(name: "SwiftGenPlugin", package: "SwiftGenPlugin")
             ]
         )
     ]
--- ios/scripts/swiftgen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh -l
-
-echo "Generating identifiers for Colors and Images"
-mint run swiftgen run xcassets "PresentationLayer/UIToolkit/Sources/UIToolkit/Resources/Colors.xcassets" "PresentationLayer/UIToolkit/Sources/UIToolkit/Resources/Images.xcassets" -p "scripts/swiftgen-xcassets.stencil" --param publicAccess --output "PresentationLayer/UIToolkit/Sources/UIToolkit/Resources/Constants/Assets.generated.swift"
-
-echo "Generating identifiers for Localizable"
-mint run swiftgen run strings "PresentationLayer/UIToolkit/Sources/UIToolkit/Resources/Localizable/Base.lproj/Localizable.strings" -p "scripts/swiftgen-strings.stencil" --param publicAccess --output "PresentationLayer/UIToolkit/Sources/UIToolkit/Resources/Constants/Localizable.generated.swift"
