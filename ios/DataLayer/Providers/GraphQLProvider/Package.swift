153c15a747fa1522c11daf7c860531e49d8b98a1
Petr Chmelar <petr.chmelar@matee.cz>
2022-10-04 16:26:08 +0100

    Update Apollo to 1.0.0 and use via SPM Plugins

--- ios/DataLayer/Providers/GraphQLProvider/Package.swift
+++ ios/DataLayer/Providers/GraphQLProvider/Package.swift
@@ -5,7 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "GraphQLProvider",
-    platforms: [.iOS(.v14)],
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v10_14)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -20,7 +23,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apollographql/apollo-ios", .upToNextMajor(from: "0.0.0"))
+        .package(url: "https://github.com/apollographql/apollo-ios", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- ios/scripts/apollo.sh
+++ ios/scripts/apollo.sh
@@ -1,32 +1,9 @@
-#!/bin/zsh -l
+#!/bin/bash
 
-# Don't run this during index builds
-if [ $ACTION = "indexbuild" ]; then exit 0; fi
+cd "$(dirname "$0")"
+cd ../DataLayer/Providers/GraphQLProvider
 
-# Go to the build root and search up the chain to find the Derived Data Path where the source packages are checked out.
-DERIVED_DATA_CANDIDATE="${BUILD_ROOT}"
-
-while ! [ -d "${DERIVED_DATA_CANDIDATE}/SourcePackages" ]; do
-  if [ "${DERIVED_DATA_CANDIDATE}" = / ]; then
-    echo >&2 "error: Unable to locate SourcePackages directory from BUILD_ROOT: '${BUILD_ROOT}'"
-    exit 1
-  fi
-
-  DERIVED_DATA_CANDIDATE="$(dirname "${DERIVED_DATA_CANDIDATE}")"
-done
-
-# Grab a reference to the directory where scripts are checked out
-SCRIPT_PATH="${DERIVED_DATA_CANDIDATE}/SourcePackages/checkouts/apollo-ios/scripts"
-
-if [ -z "${SCRIPT_PATH}" ]; then
-    echo >&2 "error: Couldn't find the CLI script in your checked out SPM packages; make sure to add the framework to your project."
-    exit 1
-fi
-
-cd "${SRCROOT}"
-
-# Download GraphQL schema
-#"${SCRIPT_PATH}"/run-bundled-codegen.sh schema:download --endpoint="https://apollo-fullstack-tutorial.herokuapp.com/graphql" "./DataLayer/schema.json"
-
-# Generate code from queries
-"${SCRIPT_PATH}"/run-bundled-codegen.sh codegen:generate --target=swift --includes="./DataLayer/Toolkits/RocketToolkit/Sources/RocketToolkit/NetworkQueries/*.graphql" --localSchemaFile="./DataLayer/schema.json" "./DataLayer/Toolkits/RocketToolkit/Sources/RocketToolkit/NetworkModels/NETModels.generated.swift"
+# RocketToolkit
+swift package --disable-sandbox --allow-writing-to-package-directory apollo-fetch-schema --path "../../Toolkits/RocketToolkit/apollo-codegen-config.json"
+swift package --disable-sandbox --allow-writing-to-package-directory apollo-generate --path "../../Toolkits/RocketToolkit/apollo-codegen-config.json"
+find ../../Toolkits/RocketToolkit/Sources/RocketToolkitMocks -type f -exec sed -i '' -e "s/rocket/Rocket/g" {} +
