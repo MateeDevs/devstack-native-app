74aa02a5affba1f7d9874d0a13a4e14c957ad288
Petr Chmelar <petr.chmelar@matee.cz>
2022-10-09 20:45:22 +0100

    Update SwiftLint to 0.50.3 and use via SPM Plugins

--- ios/scripts/setup.sh
+++ ios/scripts/setup.sh
@@ -20,15 +20,6 @@ echo "⚙️  Downloading GraphQL schemas and generating code from queries"
 echo "⚙️  Generating mocks for UseCases and Repositories"
 ./sourcery.sh
 
-echo "⚙️  Checking whether Homebrew is installed"
-if command -v brew &> /dev/null; then
-  echo "✅ Homebrew is installed"
-else
-  echo "❌ Homebrew is not installed"
-  echo "Please visit https://brew.sh for more info"
-  exit
-fi
-
 echo "⚙️  Checking whether Twine is installed"
 if command -v twine &> /dev/null; then
   echo "✅ Twine is installed"
@@ -36,15 +27,3 @@ else
   echo "❌ Twine is not installed - installing now"
   gem install twine
 fi
-
-echo "⚙️  Checking whether Mint is installed"
-if command -v mint &> /dev/null; then
-  echo "✅ Mint is installed"
-else
-  echo "❌ Mint is not installed - installing now"
-  brew install mint
-fi
-
-echo "⚙️  Bootstraping Mint dependencies"
-cd ..
-mint bootstrap
