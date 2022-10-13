38ff6bc0f288f2a5ce5b5765828a09abac495ba8
Petr Chmelar <petr.chmelar@matee.cz>
2022-10-13 19:40:23 +0100

    Fix schemaName in Apollo config

--- ios/scripts/apollo.sh
+++ ios/scripts/apollo.sh
@@ -6,5 +6,4 @@ cd "$(dirname "$0")"
 cd ../DataLayer/Toolkits/RocketToolkit
 swift package --disable-sandbox --allow-writing-to-package-directory apollo-fetch-schema
 swift package --disable-sandbox --allow-writing-to-package-directory apollo-generate
-find ./Sources/RocketToolkitMocks -type f -exec sed -i '' -e "s/rocket/Rocket/g" {} +
 cd ../../..
