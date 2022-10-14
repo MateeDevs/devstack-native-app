a9da305f6469cba7abe7322ef6eaf0451406191b
Petr Chmelar <petr.chmelar@matee.cz>
2022-10-15 00:54:06 +0100

    Switch Apollo from RocketToolkit back to GraphQLProvider

--- ios/scripts/apollo.sh
+++ ios/scripts/apollo.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
+cd ../DataLayer/Providers/GraphQLProvider
 
-# RocketToolkit
-cd ../DataLayer/Toolkits/RocketToolkit
-swift package --disable-sandbox --allow-writing-to-package-directory apollo-fetch-schema
-swift package --disable-sandbox --allow-writing-to-package-directory apollo-generate
-cd ../../..
+echo "⚙️  RocketToolkit - Downloading GraphQL schema and generating code from queries"
+swift package --disable-sandbox --allow-writing-to-package-directory apollo-fetch-schema --path "../../Toolkits/RocketToolkit/apollo-codegen-config.json"
+swift package --disable-sandbox --allow-writing-to-package-directory apollo-generate --path "../../Toolkits/RocketToolkit/apollo-codegen-config.json"
