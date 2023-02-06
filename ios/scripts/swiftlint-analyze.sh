d937c96d15f8b4bad7d29cd7601c41fce6403bb0
Petr Chmelař <petr.chmelar@matee.cz>
2023-02-06 17:35:20 +0100

    Update SwiftLint to 0.50.3 and use via SPM Plugins (#53)

--- ios/scripts/swiftlint-analyze.sh
+++ ios/scripts/swiftlint-analyze.sh
@@ -7,4 +7,4 @@ echo "Building project in order to obtain xcodebuild.log"
 xcodebuild -workspace DevStack.xcworkspace -scheme DevStack_Alpha > xcodebuild.log
 
 echo "Running SwiftLint Static Analyzer"
-mint run swiftlint analyze --config .swiftlint.yml --compiler-log-path xcodebuild.log
+swiftlint analyze --config .swiftlint.yml --compiler-log-path xcodebuild.log
--- ios/scripts/swiftlint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh -l
-
-echo "Running SwiftLint"
-mint run swiftlint autocorrect --config .swiftlint.yml
-mint run swiftlint --config .swiftlint.yml
