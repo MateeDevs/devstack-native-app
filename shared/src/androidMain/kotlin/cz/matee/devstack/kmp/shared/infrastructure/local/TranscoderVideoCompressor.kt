d1c46be3a30f3903f4ac84d8571ed4832a810383
Vojtěch Hořánek <vojtechhoranek@gmail.com>
2023-04-26 21:18:33 +0200

    Clean up transcoder

--- shared/src/androidMain/kotlin/cz/matee/devstack/kmp/shared/infrastructure/local/TranscoderVideoCompressor.kt
+++ shared/src/androidMain/kotlin/cz/matee/devstack/kmp/shared/infrastructure/local/TranscoderVideoCompressor.kt
@@ -4,7 +4,9 @@ import android.app.Application
 import androidx.core.net.toUri
 import com.otaliastudios.transcoder.Transcoder
 import com.otaliastudios.transcoder.TranscoderListener
+import com.otaliastudios.transcoder.TranscoderOptions
 import com.otaliastudios.transcoder.resize.AtMostResizer
+import com.otaliastudios.transcoder.source.DataSource
 import com.otaliastudios.transcoder.source.TrimDataSource
 import com.otaliastudios.transcoder.source.UriDataSource
 import com.otaliastudios.transcoder.strategy.DefaultVideoStrategy
@@ -13,6 +15,7 @@ import cz.matee.devstack.kmp.shared.base.error.domain.CommonError
 import cz.matee.devstack.kmp.shared.domain.model.VideoCompressOptions
 import cz.matee.devstack.kmp.shared.domain.model.VideoCompressResult
 import cz.matee.devstack.kmp.shared.system.Log
+import kotlinx.coroutines.channels.ProducerScope
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.channels.trySendBlocking
 import kotlinx.coroutines.flow.Flow
@@ -27,67 +30,73 @@ class TranscoderVideoCompressor(
         options: VideoCompressOptions,
     ): Flow<VideoCompressResult> = callbackFlow {
         Log.d("TranscoderVideoCompressor", "Compressing $inputPath to $outputPath using $options")
-        val uriDatasource = UriDataSource(application, inputPath.toUri())
+        val uriDataSource = UriDataSource(application, inputPath.toUri())
         Transcoder.into(outputPath)
-            .also { builder ->
-                if (options.trim != null) {
-                    uriDatasource.initialize()
-                    val maxDuration = uriDatasource.durationUs
-                    builder.addDataSource(
-                        TrimDataSource(
-                            uriDatasource,
-                            0,
-                            (maxDuration - options.trim.inWholeMicroseconds).coerceAtLeast(
-                                0,
-                            ),
-                        ),
-                    )
-                } else {
-                    builder.addDataSource(uriDatasource)
-                }
-            }
+            .applyTrim(options, uriDataSource)
             .setVideoTrackStrategy(
                 DefaultVideoStrategy.Builder()
-                    .also { builder ->
-                        if (options.bitrate != null) {
-                            builder.bitRate(options.bitrate)
-                        }
-                        if (options.frameRate != null) {
-                            builder.frameRate(options.frameRate)
-                        }
-                        if (options.maximumSize != null) {
-                            val major = maxOf(options.maximumSize.first, options.maximumSize.second)
-                            val minor = minOf(options.maximumSize.first, options.maximumSize.second)
-                            builder.addResizer(
-                                AtMostResizer(minor, major),
-                            )
-                        }
-                    }
+                    .applyBitrate(options)
+                    .applyFrameRate(options)
+                    .applyMaximumSize(options)
                     .build(),
             )
-            .setListener(
-                object : TranscoderListener {
-                    override fun onTranscodeProgress(progress: Double) {
-                        trySendBlocking(VideoCompressResult.Progress((progress * 100).toInt()))
-                    }
-
-                    override fun onTranscodeCompleted(successCode: Int) {
-                        trySendBlocking(VideoCompressResult.Completion(Result.Success(Unit)))
-                        close()
-                    }
-
-                    override fun onTranscodeCanceled() {
-                        trySendBlocking(VideoCompressResult.Completion(Result.Error(CommonError.UnknownError)))
-                        close()
-                    }
-
-                    override fun onTranscodeFailed(exception: Throwable) {
-                        trySendBlocking(VideoCompressResult.Completion(Result.Error(CommonError.UnknownError)))
-                        close()
-                    }
-                },
-            )
+            .setListener(flowListener)
             .transcode()
         awaitClose()
     }
+
+    private fun TranscoderOptions.Builder.applyTrim(
+        options: VideoCompressOptions,
+        dataSource: DataSource,
+    ) = apply {
+        options.trim?.let {
+            dataSource.initialize()
+            val maxDuration = dataSource.durationUs
+            addDataSource(
+                TrimDataSource(
+                    dataSource,
+                    0,
+                    (maxDuration - options.trim.inWholeMicroseconds)
+                        .coerceAtLeast(0),
+                ),
+            )
+        } ?: addDataSource(dataSource)
+    }
+
+    private fun DefaultVideoStrategy.Builder.applyBitrate(options: VideoCompressOptions) =
+        apply { options.bitrate?.let(::bitRate) }
+
+    private fun DefaultVideoStrategy.Builder.applyFrameRate(options: VideoCompressOptions) =
+        apply { options.frameRate?.let(::frameRate) }
+
+    private fun DefaultVideoStrategy.Builder.applyMaximumSize(options: VideoCompressOptions) =
+        apply {
+            options.maximumSize?.let { (first, second) ->
+                val major = maxOf(first, second)
+                val minor = minOf(first, second)
+                addResizer(AtMostResizer(minor, major))
+            }
+        }
+
+    private val ProducerScope<VideoCompressResult>.flowListener
+        get() = object : TranscoderListener {
+            override fun onTranscodeProgress(progress: Double) {
+                trySendBlocking(VideoCompressResult.Progress((progress * 100).toInt()))
+            }
+
+            override fun onTranscodeCompleted(successCode: Int) {
+                trySendBlocking(VideoCompressResult.Completion(Result.Success(Unit)))
+                close()
+            }
+
+            override fun onTranscodeCanceled() {
+                trySendBlocking(VideoCompressResult.Completion(Result.Error(CommonError.UnknownError)))
+                close()
+            }
+
+            override fun onTranscodeFailed(exception: Throwable) {
+                trySendBlocking(VideoCompressResult.Completion(Result.Error(CommonError.UnknownError)))
+                close()
+            }
+        }
 }
