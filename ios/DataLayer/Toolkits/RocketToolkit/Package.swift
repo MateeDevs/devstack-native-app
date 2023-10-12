d9936d3315163237d20075985558b3ff9c57e2e0
Petr Chmelar <petr.chmelar@matee.cz>
2023-10-12 12:03:41 +0200

    [iOS] Update Apollo to 1.6.0

--- ios/DataLayer/Toolkits/RocketToolkit/Package.swift
+++ ios/DataLayer/Toolkits/RocketToolkit/Package.swift
@@ -18,7 +18,8 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(name: "SharedDomain", path: "../../../DomainLayer/SharedDomain"),
         .package(name: "GraphQLProvider", path: "../../Providers/GraphQLProvider"),
-        .package(url: "https://github.com/apollographql/apollo-ios", .upToNextMajor(from: "1.0.0"))
+        .package(url: "https://github.com/apollographql/apollo-ios", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/apollographql/apollo-ios-codegen", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
