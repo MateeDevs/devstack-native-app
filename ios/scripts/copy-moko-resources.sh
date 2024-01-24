84c8cb0e4fc54b44dee4c21271e9a5bf3ae27865
JuliaJakubcova <jakubcovajulia@gmail.com>
2024-01-24 09:06:58 +0100

    [iOS] Fix xcode configuration for copy moko resources script

--- ios/scripts/copy-moko-resources.sh
+++ ios/scripts/copy-moko-resources.sh
@@ -1,6 +1,5 @@
 #!/bin/zsh -l
 
-# This code is not valid - should be refactored after fix issue with static framework
 configurationWordCount=$(echo "$CONFIGURATION" | wc -w)
 if (( configurationWordCount > 1 ))
 then
@@ -9,17 +8,9 @@ else
   buildType="$CONFIGURATION"
 fi
 
-
-#"$SRCROOT/../gradlew" -p "$SRCROOT/../" :shared:copyFrameworkResourcesToApp \
-#-Pmoko.resources.PLATFORM_NAME="$PLATFORM_NAME" \
-#-Pmoko.resources.CONFIGURATION="$buildType"  \
-#-Pmoko.resources.ARCHS="$ARCHS" \
-#-Pmoko.resources.BUILT_PRODUCTS_DIR="$BUILT_PRODUCTS_DIR" \
-#-Pmoko.resources.CONTENTS_FOLDER_PATH="$CONTENTS_FOLDER_PATH" \
-#-PXCODE_CONFIGURATION="$CONFIGURATION"
-
 # This is required step for DevstackKmpShared framework
 # https://github.com/icerockdev/moko-resources#creating-xcframework-with-resources
 "$SRCROOT/../gradlew" -p "$SRCROOT/../" :shared:copyResourcesDevstackKmpShared${buildType}XCFrameworkToApp \
     -Pmoko.resources.BUILT_PRODUCTS_DIR=$BUILT_PRODUCTS_DIR \
-    -Pmoko.resources.CONTENTS_FOLDER_PATH=$CONTENTS_FOLDER_PATH
+    -Pmoko.resources.CONTENTS_FOLDER_PATH=$CONTENTS_FOLDER_PATH \
+    -PXCODE_CONFIGURATION="$CONFIGURATION"
