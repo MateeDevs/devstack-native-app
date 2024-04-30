c4768e760542774b120116bac5218f249ca8c5d0
Petr Chmelar <petr.chmelar@matee.cz>
2024-04-30 15:33:36 +0100

    [iOS] Define MinimumOSVersion in KMP

--- build-logic/convention/src/main/kotlin/plugin/KmmLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/plugin/KmmLibraryConventionPlugin.kt
@@ -102,6 +102,13 @@ class KmmLibraryConventionPlugin : Plugin<Project> {
                 configureTests()
             }
 
+            // There is a bug in Xcode 15.3 - archive validation fails if SDKs support a lower minimum OS version than the app 
+            // Konan plugin sets minVersion.ios to 12.0 by default, but we have 15.0 in our app
+            // This line changes Konan property to 15.0
+            tasks.withType<KotlinNativeLink> {
+                kotlinOptions.freeCompilerArgs += "-Xoverride-konan-properties=minVersion.ios=15.0"
+            }
+
             tasks.register("buildXCFramework") {
                 dependsOn("assemble${ProjectConstants.iosShared}XCFramework")
             }
